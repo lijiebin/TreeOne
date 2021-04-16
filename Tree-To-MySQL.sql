ea764196bfe2cce49f2a24a654fc799d8916653e
JohnBeanLee <278636108@qq.com>
2021-04-16 17:12:20 +0800

    Create Tree-To-MySQL.sql

---
+++ Tree-To-MySQL.sql
@@ -0,0 +1,30 @@
+CREATE TABLE `catlog` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `level` mediumint(4) NOT NULL DEFAULT '1',
+  `branch` mediumint(4) NOT NULL DEFAULT '0',
+  `path` varchar(50) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+INSERT INTO `catlog` (`id`, `name`, `level`, `branch`, `path`) VALUES
+(1, 'cate-1', 1, 0, '0'),
+(2, 'cate-2', 1, 1, '0'),
+(3, 'cate-1-1', 2, 0, '1.1'),
+(4, 'cate-1-1-1', 3, 0, '1.1.1'),
+(5, 'cate-1-2', 2, 0, '1.2'),
+(6, 'cate-1-2-1', 3, 0, '1.2.1'),
+(7, 'cate-2-1', 2, 1, '2.1'),
+(8, 'cate-2-2', 2, 1, '2.2'),
+(9, 'cate-2-1-1', 3, 1, '2.1.1'),
+(10, 'cate-2-1-2', 3, 1, '2.1.2');
+
+
+ALTER TABLE `catlog`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `rank` (`branch`,`path`),
+  ADD KEY `level` (`level`);
+
+
+ALTER TABLE `catlog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
