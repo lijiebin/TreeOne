855e828352e18a9e2c327cb054f34ada00f04870
JohnBeanLee <278636108@qq.com>
2021-04-17 18:24:24 +0800

    Update Tree-To-MySQL-Testing.sql

--- Tree-To-MySQL-Testing.sql
+++ Tree-To-MySQL-Testing.sql
@@ -2,22 +2,22 @@ CREATE TABLE `tree` (
   `id` int(11) NOT NULL,
   `name` varchar(30) NOT NULL,
   `level` mediumint(4) NOT NULL DEFAULT '1',
-  `branch` mediumint(4) NOT NULL DEFAULT '0',
-  `path` varchar(50) NOT NULL DEFAULT '0'
+  `branch` mediumint(4) NOT NULL DEFAULT '1',
+  `path` varchar(50) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 INSERT INTO `tree` (`id`, `name`, `level`, `branch`, `path`) VALUES
-(1, 'cate-1', 1, 0, '0'),
-(2, 'cate-2', 1, 1, '0'),
-(3, 'cate-1-1', 2, 0, '1.1'),
-(4, 'cate-1-1-1', 3, 0, '1.1.1'),
-(5, 'cate-1-2', 2, 0, '1.2'),
-(6, 'cate-1-2-1', 3, 0, '1.2.1'),
-(7, 'cate-2-1', 2, 1, '2.1'),
-(8, 'cate-2-2', 2, 1, '2.2'),
-(9, 'cate-2-1-1', 3, 1, '2.1.1'),
-(10, 'cate-2-1-2', 3, 1, '2.1.2');
+(1, 'cate-1', 1, 1, '1'),
+(2, 'cate-2', 1, 2, '2'),
+(3, 'cate-1-1', 2, 1, '1.1'),
+(4, 'cate-1-1-1', 3, 1, '1.1.1'),
+(5, 'cate-1-2', 2, 1, '1.2'),
+(6, 'cate-1-2-1', 3, 1, '1.2.1'),
+(7, 'cate-2-1', 2, 2, '2.1'),
+(8, 'cate-2-2', 2, 2, '2.2'),
+(9, 'cate-2-1-1', 3, 2, '2.1.1'),
+(10, 'cate-2-1-2', 3, 2, '2.1.2');
 
 
 ALTER TABLE `tree`
